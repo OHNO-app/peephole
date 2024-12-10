13a2f879351a03ba920bda6453b8ff81dfbcd1e6
Jasper Kennis <contact@jasperkennis.nl>
2024-12-10 14:15:49 +0100

    style: Remove default page padding to use full viewport width

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Peephole from './components/Peephole';
 
+// The page has a small padding all around it, can we make this 100% of the viewport? AI!
 function App() {
   return (
     <div style={{
